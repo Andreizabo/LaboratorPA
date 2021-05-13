3e5c53af850fdc948e7be5f0b1298bddc8804644
vladvlad00 <teodorescu.vlad@yahoo.com>
2021-05-13 13:18:51 +0300

    add lab 11 compulsory

--- Laborator11PA/src/main/java/compulsory/entity/Person.java
+++ Laborator11PA/src/main/java/compulsory/entity/Person.java
@@ -13,4 +13,24 @@ public class Person
     Long id;
 
     String name;
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
 }
--- Laborator11PA/src/main/java/compulsory/repository/PersonRepository.java
+++ Laborator11PA/src/main/java/compulsory/repository/PersonRepository.java
@@ -1,5 +1,8 @@
 package compulsory.repository;
 
-public class PersonRepository
+import compulsory.entity.Person;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonRepository extends CrudRepository<Person, Long>
 {
 }
