ce1e89b8f9b60cab8b8bc72881ccd2bbceebf269
vladvlad00 <teodorescu.vlad@yahoo.com>
2021-05-13 12:37:06 +0300

    fix project

---
+++ Laborator11PA/src/main/java/compulsory/CompulsoryApplication.java
@@ -0,0 +1,13 @@
+package compulsory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompulsoryApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(CompulsoryApplication.class, args);
+	}
+}
